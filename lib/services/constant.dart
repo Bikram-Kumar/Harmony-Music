c61fd03011a844f203e04871bdb500cb8b0ebb1c
Bkram Kumar <coolbikram0@gmail.com>
2026-08-31 09:43:52 +0530

    google fonts update and client name to mac

--- lib/services/constant.dart
+++ lib/services/constant.dart
@@ -3,4 +3,4 @@ const String baseUrl = '${domain}youtubei/v1/';
 const fixedParms =
     '?prettyPrint=false&alt=json&key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30';
 const userAgent =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 15_7_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15';
--- lib/ui/screens/Settings/settings_screen_controller.dart
+++ lib/ui/screens/Settings/settings_screen_controller.dart
@@ -47,7 +47,7 @@ class SettingsScreenController extends GetxController {
   final keepScreenAwake = false.obs;
   final restorePlaybackSession = false.obs;
   final cacheHomeScreenData = true.obs;
-  final currentVersion = "V1.14.0";
+  final currentVersion = "V1.14.1";
 
   @override
   void onInit() {
